ed89652bb65def27236fb6496b538c7aaa137164
ashur-k <ashurkanwal@yahoo.com>
2021-03-02 20:08:07 +0000

    deploying

--- eCommerce_Project/settings.py
+++ eCommerce_Project/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('COMMERCIAL_ECOMMERCE_SECRET_KEY', '')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['jacxx-productions.herokuapp.com', 'localhost']
