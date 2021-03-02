8d7fdca5a2ff6edca432ae047e11af318ba66bd0
ashur-k <ashurkanwal@yahoo.com>
2021-03-02 19:20:15 +0000

    deploying site

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Jacxx_Productions.wsgi:application
--- eCommerce_Project/settings.py
+++ eCommerce_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('COMMERCIAL_ECOMMERCE_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'jacxx-productions.herokuapp.com']
 
 
 # Application definition
@@ -116,13 +116,17 @@ WSGI_APPLICATION = 'eCommerce_Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
