d62f4f670a48bda23be76a22e94ec27e94347387
ashur-k <ashurkanwal@yahoo.com>
2021-03-04 10:18:18 +0000

    added video

--- videos/views.py
+++ videos/views.py
@@ -4,10 +4,10 @@ from .models import Video
 
 def videos_home(request):
     videos = Video.objects.all()
-    video = get_object_or_404(Video, id=1)
+    my_video = get_object_or_404(Video, id=1)
     template = 'videos/videos_home.html'
     context = {
         'videos': videos,
-        'video': video,
+        'my_video': my_video,
     }
     return render(request, template , context)
