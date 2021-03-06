0f28adf60dd88dcc20892a5f3af82ece6efbe3fb
ashur-k <ashurkanwal@yahoo.com>
2021-03-06 15:35:19 +0000

    navbar redesigned

--- Jaxx_products_home/contexts.py
+++ Jaxx_products_home/contexts.py
@@ -1,13 +1,15 @@
 from products.models import Category
+from videos.models import VideoCategory
 
 
 def navbar_categories(request):
-    clothing_categories = Category.objects.filter(parent_name='clothing')
-    homeware_categories = Category.objects.filter(parent_name='homeware')
-    
+    #clothing_categories = Category.objects.filter(parent_name='clothing')
+    #homeware_categories = Category.objects.filter(parent_name='homeware')
+    video_categories = VideoCategory.objects.all()
     context = {
-        'clothing_categories': clothing_categories,
-        'homeware_categories': homeware_categories,
+        # 'clothing_categories': clothing_categories,
+        # 'homeware_categories': homeware_categories,
+        'video_categories': video_categories
     }
 
     return context
