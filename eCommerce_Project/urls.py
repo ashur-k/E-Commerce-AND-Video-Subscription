07d2ca0c6bc78f56a22ccb738fdcabd52100c790
ashur-k <ashurkanwal@yahoo.com>
2021-03-09 15:23:34 +0000

    updated home page

--- eCommerce_Project/urls.py
+++ eCommerce_Project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('Jaxx_products_home.urls')),
+    path('home/', include('Jaxx_products_home.urls')),
     path('products/', include('products.urls')),
-    path('videos/', include('videos.urls')),
+    path('', include('videos.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- videos/urls.py
+++ videos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('videos_home', views.videos_home, name="videos_home")
+    path('', views.videos_home, name="videos_home")
 ]
